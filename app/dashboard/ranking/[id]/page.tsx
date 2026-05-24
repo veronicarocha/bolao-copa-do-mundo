a2aa262c192d4b88ab79ad436b01ab3423efd0f2
Veronica Rocha <vedarocha@gmail.com>
2026-05-24 20:51:44 -0300

    ajuste caminho de arquivo

--- app/dashboard/ranking/[id]/page.tsx
+++ app/dashboard/ranking/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../../../../lib/supabaseClient'
 import { notFound } from 'next/navigation';
 
 interface DetalhePerfil {
